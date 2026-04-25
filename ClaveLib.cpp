4b3e775ae39d46644957680667dc6607bac85f89
samanthacastrorajbe <samanthacastrorajbe@gmail.com>
2026-04-25 18:01:01 -0400

    Update ClaveLib.cpp

--- ClaveLib.cpp
+++ ClaveLib.cpp
@@ -1,1 +1,11 @@
 #include "ClaveLib.h"
+#include <string>
+#include <iostream>
+
+ClaveLib crear_clave_lib();
+
+std::string autor_clave (const ClaveLib& clave);
+
+std::string titulo_clave (const ClaveLib& clave);
+
+unsigned short compara_claves(const ClaveLib& clave1, const ClaveLib& clave2);

bbd670f89890a85fa4d22f3d8e218845c2e584ad
samanthacastrorajbe <samanthacastrorajbe@gmail.com>
2026-04-29 10:23:39 -0400

    struct NodoLibro y struct Libros en Libros.h

--- Area.h
+++ Area.h
@@ -1,5 +1,12 @@
 #ifndef AREA_H 
 #define AREA_H
 #include <iostream>
+#include "Libros.h"
+
+struct Area {
+    std:: string codigo_area;
+    Libros libros;
+};
+
 
 #endif
--- Libro.cpp
+++ Libro.cpp
@@ -2,19 +2,28 @@
 
 
 std::string autor_libro (const Libro& libro);
+// desarrollar
 
 std::string titulo_libro (const Libro& libro); 
+// desarrollar
 
 std::string año_libro (const Libro& libro); 
+// desarrollar
 
 std::string area_libro (const Libro& libro);
+// desarrollar
 
 unsigned short num_ejem (const Libro& libro); 
+// desarrollar
 
 ClaveLibro clave_libro (const Libro& libro); 
+// desarrollar
 
 void num_ejem (const Libro& libro, unsigned short ejem); 
+// desarrollar
 
 bool es_clave (const Libro& libro, ClaveLibro clave); 
+// desarrollar
 
 void destruir_libro (Libro& libro);
+// desarrollar
